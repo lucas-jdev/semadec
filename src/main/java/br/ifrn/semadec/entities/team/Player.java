605df5c1e39ca3e7e7a61942d10c7149a3f19d8f
Lucas Jdev <lucas.jdev1@gmail.com>
2022-10-18 00:06:49 -0300

    entidade player

---
+++ src/main/java/br/ifrn/semadec/entities/team/Player.java
@@ -0,0 +1,25 @@
+package br.ifrn.semadec.entities.team;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import br.ifrn.semadec.entities.user.User;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Data
+@Builder
+@Table(name = "players")
+public class Player {
+
+    @Id
+    @OneToOne
+    private User user;
+
+    private int shirtNumber;
+    private String position;
+
+}
