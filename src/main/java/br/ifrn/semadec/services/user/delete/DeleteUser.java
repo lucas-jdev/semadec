6a90e2ae0889e788dd103f7fb7ccdc1e4e3eb599
Lucas Jdev <lucas.jdev1@gmail.com>
2022-10-18 06:16:38 -0300

    delete user

---
+++ src/main/java/br/ifrn/semadec/services/user/delete/DeleteUser.java
@@ -0,0 +1,27 @@
+package br.ifrn.semadec.services.user.delete;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.ifrn.semadec.entities.user.User;
+import br.ifrn.semadec.repositories.UserRepository;
+
+@Service
+public class DeleteUser {
+
+    @Autowired
+    private static UserRepository userRepository;
+
+    private DeleteUser() {
+        throw new IllegalStateException("Service class");
+    }
+
+    public static void execute(final UUID id) {
+        Optional<User> findById = userRepository.findById(id);
+        findById.ifPresent(User::inactive);
+        findById.ifPresent(userRepository::save);
+    }
+}
