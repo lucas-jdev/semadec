6f0d267a49932e44818c3bd40f2df297b12b82cf
Lucas Jdev <lucas.jdev1@gmail.com>
2022-10-18 06:17:22 -0300

    login do usuário

---
+++ src/main/java/br/ifrn/semadec/services/user/login/LoginUser.java
@@ -0,0 +1,34 @@
+package br.ifrn.semadec.services.user.login;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.ifrn.semadec.entities.user.User;
+import br.ifrn.semadec.repositories.UserRepository;
+
+@Service
+public class LoginUser {
+
+    @Autowired
+    private static UserRepository userRepository;
+
+    private LoginUser() {
+        throw new IllegalStateException("Service class");
+    }
+
+    public static void execute(final String username, final String password) {
+        final var user = username.toLowerCase().trim();
+        final var pass = password.trim();
+        final User userFound;
+        userFound = userRepository.findByUsernameAndPassword(user, pass);
+        _validateUser(userFound);
+    }
+
+    private static void _validateUser(final User userFound) {
+        boolean userNotFound = userFound == null;
+        if (userNotFound) {
+            throw new RuntimeException("User not found");
+        }
+    }
+
+}
