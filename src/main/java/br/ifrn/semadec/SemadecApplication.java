3cc18102a7953cc114dfc876b850ea0648d56363
Lucas Jdev <lucas.jdev1@gmail.com>
2022-10-17 21:46:08 -0300

    first commit

---
+++ src/main/java/br/ifrn/semadec/SemadecApplication.java
@@ -0,0 +1,13 @@
+package br.ifrn.semadec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemadecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemadecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ifrn/semadec/SemadecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifrn.semadec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemadecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
