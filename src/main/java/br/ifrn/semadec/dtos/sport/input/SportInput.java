4d63ccf409e3b130569294de1328cac71a022c40
Lucas Jdev <lucas.jdev1@gmail.com>
2022-10-18 06:21:31 -0300

    input sport

---
+++ src/main/java/br/ifrn/semadec/dtos/sport/input/SportInput.java
@@ -0,0 +1,16 @@
+package br.ifrn.semadec.dtos.sport.input;
+
+import br.ifrn.semadec.dtos.course.input.CourseInput;
+import br.ifrn.semadec.entities.sport.CategoryGender;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class SportInput {
+
+    private String name;
+    private CategoryGender categoryGender;
+    private String categorySport;
+    private CourseInput courseInput;
+}
