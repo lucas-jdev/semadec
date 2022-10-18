20db0904024fc3bc47ea5ba9aa57aa7e0c28872c
Lucas Jdev <lucas.jdev1@gmail.com>
2022-10-17 22:41:12 -0300

    entidade flag

---
+++ src/main/java/br/ifrn/semadec/entities/flag/Flag.java
@@ -0,0 +1,28 @@
+package br.ifrn.semadec.entities.flag;
+
+import java.util.Set;
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import br.ifrn.semadec.entities.user.User;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Data
+@Builder
+public class Flag {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String name;
+
+    @ManyToMany(mappedBy = "flags")
+    private Set<User> users;
+
+}
