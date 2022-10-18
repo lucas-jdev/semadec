4ff921c56827eb9dce12c5ce51fe643350dd8f3d
Lucas Jdev <lucas.jdev1@gmail.com>
2022-10-18 00:15:29 -0300

    entidade game

---
+++ src/main/java/br/ifrn/semadec/entities/game/Game.java
@@ -0,0 +1,47 @@
+package br.ifrn.semadec.entities.game;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import br.ifrn.semadec.entities.score.Score;
+import br.ifrn.semadec.entities.sport.Sport;
+import br.ifrn.semadec.entities.team.Team;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Data
+@Builder
+@Table(name = "games")
+public class Game {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(columnDefinition = "timestamp")
+    private LocalDateTime date;
+
+    @Enumerated(EnumType.STRING)
+    private StatusGame status;
+
+    @OneToOne
+    private Sport sport;
+
+    @OneToOne
+    private List<Team> teams;
+
+    @OneToOne
+    private List<Score> scores;
+
+}
