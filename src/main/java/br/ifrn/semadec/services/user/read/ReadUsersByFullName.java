be593c2758d76ed2ce2f1e8f18c687c4f4989220
Lucas Jdev <lucas.jdev1@gmail.com>
2022-10-23 18:44:23 -0300

    mudança de iterable para collection

--- src/main/java/br/ifrn/semadec/services/user/read/ReadUsersByFullName.java
+++ src/main/java/br/ifrn/semadec/services/user/read/ReadUsersByFullName.java
@@ -1,5 +1,7 @@
 package br.ifrn.semadec.services.user.read;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +18,7 @@ public class ReadUsersByFullName {
         throw new IllegalStateException("Service class");
     }
 
-    public static Iterable<User> execute(final String fullName) {
+    public static Collection<User> execute(final String fullName) {
         return userRepository.findByFullName(fullName);
     }
 
