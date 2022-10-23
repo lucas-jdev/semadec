ee51b6abd083033aede5daf371cdcd0c082c533a
Lucas Jdev <lucas.jdev1@gmail.com>
2022-10-23 14:29:53 -0300

    lógica de update para usuário

--- src/main/java/br/ifrn/semadec/services/user/update/UpdateUser.java
+++ src/main/java/br/ifrn/semadec/services/user/update/UpdateUser.java
@@ -1,9 +1,13 @@
 package br.ifrn.semadec.services.user.update;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.ifrn.semadec.dtos.user.input.UserInput;
 import br.ifrn.semadec.entities.user.User;
+import br.ifrn.semadec.exceptions.not_found.UserNotFoundException;
 import br.ifrn.semadec.repositories.UserRepository;
 
 @Service
@@ -16,8 +20,17 @@ public class UpdateUser {
         throw new IllegalStateException("Service class");
     }
 
-    public static User execute(final User user) {
-        return userRepository.saveAndFlush(user);
+    public static User execute(final UUID userId, final UserInput input) {
+        return userRepository.findById(userId).map(user -> {
+            user.setUsername(input.getUsername());
+            user.setFullName(input.getFullName());
+            user.setCpf(input.getCpf());
+            user.setAcademicEmail(input.getAcademicEmail());
+            user.setPersonalEmail(input.getPersonalEmail());
+            user.setPhone(input.getPhone());
+            user.setMatriculation(input.getMatriculation());
+            return userRepository.save(user);
+        }).orElseThrow(UserNotFoundException::new);
     }
 
 }
