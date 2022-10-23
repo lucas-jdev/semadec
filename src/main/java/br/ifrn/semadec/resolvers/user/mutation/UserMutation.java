5ce44a5abf08d3ad58b18a0bad37615c9d4c43a6
Lucas Jdev <lucas.jdev1@gmail.com>
2022-10-23 14:30:58 -0300

    consumo de serviço de update de usuário

--- src/main/java/br/ifrn/semadec/resolvers/user/mutation/UserMutation.java
+++ src/main/java/br/ifrn/semadec/resolvers/user/mutation/UserMutation.java
@@ -1,10 +1,15 @@
 package br.ifrn.semadec.resolvers.user.mutation;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Controller;
 
 import br.ifrn.semadec.dtos.user.input.UserInput;
 import br.ifrn.semadec.dtos.user.output.UserOutput;
+import br.ifrn.semadec.entities.user.User;
 import br.ifrn.semadec.services.user.create.CreateUser;
+import br.ifrn.semadec.services.user.update.UpdateUser;
+import br.ifrn.semadec.utils.converter.ConvertEntityToDTO;
 
 @Controller
 public class UserMutation {
@@ -12,4 +17,10 @@ public class UserMutation {
     public UserOutput save(final UserInput input) {
         return CreateUser.execute(input);
     }
+
+    public UserOutput update(final String id, final UserInput input) {
+        User user = UpdateUser.execute(UUID.fromString(id), input);
+        return (UserOutput) ConvertEntityToDTO.convert(user, UserOutput.class);
+    }
+
 }
