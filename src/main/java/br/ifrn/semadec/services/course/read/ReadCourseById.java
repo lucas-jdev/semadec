0eb70cb00e8e12e3288c2b4126c4ff29d1b878e7
Lucas Jdev <lucas.jdev1@gmail.com>
2022-10-24 13:38:03 -0300

    serviço de buscar curso por id

---
+++ src/main/java/br/ifrn/semadec/services/course/read/ReadCourseById.java
@@ -0,0 +1,27 @@
+package br.ifrn.semadec.services.course.read;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.ifrn.semadec.entities.course.Course;
+import br.ifrn.semadec.exceptions.not_found.CourseNotFoundException;
+import br.ifrn.semadec.repositories.CourseRepository;
+
+@Service
+public class ReadCourseById {
+
+    @Autowired
+    private static CourseRepository courseRepository;
+
+    private ReadCourseById() {
+        throw new IllegalStateException("Service class");
+    }
+
+    public static Course execute(final UUID id) {
+        return courseRepository.findById(id)
+                .orElseThrow(CourseNotFoundException::new);
+    }
+
+}
