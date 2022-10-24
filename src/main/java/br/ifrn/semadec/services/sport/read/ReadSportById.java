af8a13607d33f2b412f405573209211dbd405998
Lucas Jdev <lucas.jdev1@gmail.com>
2022-10-24 13:50:02 -0300

    serviço de buscar esporte por id

---
+++ src/main/java/br/ifrn/semadec/services/sport/read/ReadSportById.java
@@ -0,0 +1,27 @@
+package br.ifrn.semadec.services.sport.read;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.ifrn.semadec.entities.sport.Sport;
+import br.ifrn.semadec.exceptions.not_found.SportNotFoundException;
+import br.ifrn.semadec.repositories.SportRepository;
+
+@Service
+public class ReadSportById {
+
+    @Autowired
+    private static SportRepository sportRepository;
+
+    private ReadSportById() {
+        throw new IllegalStateException("Service class");
+    }
+
+    public static Sport execute(final UUID id) {
+        return sportRepository.findById(id)
+                .orElseThrow(SportNotFoundException::new);
+    }
+
+}
