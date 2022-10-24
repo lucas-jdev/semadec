f7c93c28a5470196be93b4954a8621346a2225e1
Lucas Jdev <lucas.jdev1@gmail.com>
2022-10-24 13:49:15 -0300

    serviço de criar esporte

---
+++ src/main/java/br/ifrn/semadec/services/sport/create/CreateSport.java
@@ -0,0 +1,33 @@
+package br.ifrn.semadec.services.sport.create;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.ifrn.semadec.dtos.sport.input.SportInput;
+import br.ifrn.semadec.entities.sport.Sport;
+import br.ifrn.semadec.repositories.SportRepository;
+
+@Service
+public class CreateSport {
+
+    @Autowired
+    private static SportRepository sportRepository;
+
+    private CreateSport() {
+        throw new IllegalStateException("Service class");
+    }
+
+    public static Sport execute(final SportInput input) {
+        final Sport sport = _createSportWithDTO(input);
+        return sportRepository.save(sport);
+    }
+
+    public static Sport _createSportWithDTO(SportInput input) {
+        return Sport.builder()
+                .name(input.getName())
+                .categorySport(input.getCategorySport())
+                .categoryGender(input.getCategoryGender())
+                .build();
+    }
+
+}
