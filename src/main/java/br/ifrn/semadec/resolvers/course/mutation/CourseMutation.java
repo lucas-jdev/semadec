af0c155873b08ad6f4f86cb9c0af8844d3ce8efe
Lucas Jdev <lucas.jdev1@gmail.com>
2022-11-02 15:04:50 -0300

    mutation graphql curso

---
+++ src/main/java/br/ifrn/semadec/resolvers/course/mutation/CourseMutation.java
@@ -0,0 +1,27 @@
+package br.ifrn.semadec.resolvers.course.mutation;
+
+import java.util.UUID;
+
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.stereotype.Controller;
+
+import br.ifrn.semadec.dtos.course.input.CourseInput;
+import br.ifrn.semadec.entities.course.Course;
+import br.ifrn.semadec.services.course.create.CreateCourse;
+import br.ifrn.semadec.services.course.update.UpdateCourse;
+
+@Controller
+public class CourseMutation {
+
+    @MutationMapping
+    public Course createCourse(CourseInput input) {
+        return CreateCourse.execute(input);
+    }
+
+    @MutationMapping
+    public Course updateCourse(String id, CourseInput input) {
+        final var uuid = UUID.fromString(id);
+        return UpdateCourse.execute(uuid, input);
+    }
+
+}
