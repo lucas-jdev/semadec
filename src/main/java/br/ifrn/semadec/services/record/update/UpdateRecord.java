7825b8c1a7bd2351bbb0448c661317d2249b77a9
Lucas Jdev <lucas.jdev1@gmail.com>
2022-11-02 14:49:51 -0300

    serviço de atualização de recorde

---
+++ src/main/java/br/ifrn/semadec/services/record/update/UpdateRecord.java
@@ -0,0 +1,99 @@
+package br.ifrn.semadec.services.record.update;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.ifrn.semadec.dtos.record.input.RecordInput;
+import br.ifrn.semadec.dtos.team.input.TeamIdInput;
+import br.ifrn.semadec.entities.course.Course;
+import br.ifrn.semadec.entities.record.Record;
+import br.ifrn.semadec.entities.sport.Sport;
+import br.ifrn.semadec.entities.team.Player;
+import br.ifrn.semadec.entities.team.Team;
+import br.ifrn.semadec.entities.team.TeamId;
+import br.ifrn.semadec.exceptions.not_found.RecordNotFoundException;
+import br.ifrn.semadec.repositories.CourseRepository;
+import br.ifrn.semadec.repositories.PlayerRepository;
+import br.ifrn.semadec.repositories.RecordRepository;
+import br.ifrn.semadec.repositories.SportRepository;
+import br.ifrn.semadec.repositories.TeamRepository;
+
+@Service
+public class UpdateRecord {
+
+    @Autowired
+    private static RecordRepository recordRepository;
+
+    @Autowired
+    private static SportRepository sportRepository;
+
+    private static PlayerRepository playerRepository;
+
+    @Autowired
+    private static TeamRepository teamRepository;
+
+    @Autowired
+    private static CourseRepository courseRepository;
+
+    private UpdateRecord() {
+        throw new IllegalStateException("Service class");
+    }
+
+    public static Record execute(UUID id, RecordInput input) {
+        final Record recordFound = recordRepository.findById(id)
+                .orElseThrow(RecordNotFoundException::new);
+
+        _builderRecord(recordFound, input);
+
+        return recordRepository.save(recordFound);
+
+    }
+
+    private static Team _buildTeamByInput(RecordInput input) {
+        final var teamIdInputOptional = Optional.ofNullable(input.getTeamId());
+        TeamId teamId = teamIdInputOptional.map(teamIdInput -> {
+            return _builderTeamId(teamIdInput);
+        }).orElseThrow();
+        return teamRepository.findById(teamId).orElseThrow();
+    }
+
+    private static TeamId _builderTeamId(final TeamIdInput teamIdInput) {
+        final UUID sportId = teamIdInput.getSportId();
+        final UUID courseId = teamIdInput.getCourseId();
+
+        final var sport = sportRepository.findById(sportId).orElseThrow();
+        final var course = courseRepository.findById(courseId).orElseThrow();
+
+        return TeamId.builder()
+                .sport(sport)
+                .course(course)
+                .build();
+    }
+
+    private static Record _builderRecord(final Record recordFound, final RecordInput input) {
+        final var sportId = UUID.fromString(input.getSportId());
+        final var playerId = UUID.fromString(input.getPlayerId());
+        final var courseId = UUID.fromString(input.getCourseId());
+
+        final Sport sport = sportRepository.findById(sportId).orElseThrow();
+        final Player player = playerRepository.findById(playerId);
+        final Course course = courseRepository.findById(courseId).orElseThrow();
+        final Team team = _buildTeamByInput(input);
+
+        final var date = LocalDateTime.parse(input.getDateTime());
+
+        recordFound.setSport(sport);
+        recordFound.setPlayer(player);
+        recordFound.setCourse(course);
+        recordFound.setTeam(team);
+        recordFound.setDate(date);
+        recordFound.setScore(input.getScore());
+        recordFound.setDescription(input.getDescription());
+
+        return null;
+    }
+}
