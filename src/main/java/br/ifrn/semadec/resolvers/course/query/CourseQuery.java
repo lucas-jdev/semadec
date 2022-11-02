58f345f6dd994cc86e4ec71b4d4e50fe0f0ccb2b
Lucas Jdev <lucas.jdev1@gmail.com>
2022-11-02 15:04:34 -0300

    query graphql curso

---
+++ src/main/java/br/ifrn/semadec/resolvers/course/query/CourseQuery.java
@@ -0,0 +1,32 @@
+package br.ifrn.semadec.resolvers.course.query;
+
+import java.util.UUID;
+
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import br.ifrn.semadec.entities.course.Course;
+import br.ifrn.semadec.services.course.read.ReadAllCourses;
+import br.ifrn.semadec.services.course.read.ReadCourseById;
+import br.ifrn.semadec.services.course.read.ReadCoursesByName;
+
+@Controller
+public class CourseQuery {
+
+    @QueryMapping
+    public Iterable<Course> findAll() {
+        return ReadAllCourses.execute();
+    }
+
+    @QueryMapping
+    public Course findById(String id) {
+        final var uuid = UUID.fromString(id);
+        return ReadCourseById.execute(uuid);
+    }
+
+    @QueryMapping
+    public Iterable<Course> findByName(String name) {
+        return ReadCoursesByName.execute(name);
+    }
+
+}
