1adcd19b569d7ee059d6ecb6102df919d9662314
Lucas Jdev <lucas.jdev1@gmail.com>
2022-11-02 14:57:12 -0300

    serviço de atualização de uma edição

---
+++ src/main/java/br/ifrn/semadec/services/edition/update/UpdateEdition.java
@@ -0,0 +1,53 @@
+package br.ifrn.semadec.services.edition.update;
+
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.ifrn.semadec.entities.record.Record;
+import br.ifrn.semadec.dtos.edition.input.EditionInput;
+import br.ifrn.semadec.entities.edition.Edition;
+import br.ifrn.semadec.exceptions.not_found.EditionNotFoundException;
+import br.ifrn.semadec.repositories.EditionRepository;
+import br.ifrn.semadec.repositories.RecordRepository;
+
+@Service
+public class UpdateEdition {
+
+    @Autowired
+    private static EditionRepository editionRepository;
+
+    @Autowired
+    private static RecordRepository recordRepository;
+
+    private UpdateEdition() {
+        throw new IllegalStateException("Service class");
+    }
+
+    public static void execute(UUID id, EditionInput editionInput) {
+        Edition edition = editionRepository.findById(id)
+                .orElseThrow(EditionNotFoundException::new);
+
+        LocalDate startDate = LocalDate.parse(editionInput.getStartDate());
+
+        Set<Record> records = _getRecords(editionInput);
+
+        edition.setName(editionInput.getName());
+        edition.setStartDate(startDate);
+        edition.setRecords(records);
+
+        editionRepository.save(edition);
+    }
+
+    private static Set<Record> _getRecords(EditionInput editionInput) {
+        return editionInput.getRecords().stream().map(recordId -> {
+            UUID uuid = UUID.fromString(recordId);
+            return recordRepository.findById(uuid).get();
+        }).collect(Collectors.toSet());
+    }
+
+}
