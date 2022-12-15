c4c8aa1b7dfb0557b3734cf6b7fff80a70076a08
Lucas_Jdev <lucas.jdev1@gmail.com>
2022-12-15 14:44:27 -0300

    adição de autowired

--- src/main/java/br/ifrn/semadec/services/user/read/ReadAllUsers.java
+++ src/main/java/br/ifrn/semadec/services/user/read/ReadAllUsers.java
@@ -2,6 +2,7 @@ package br.ifrn.semadec.services.user.read;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.ifrn.semadec.entities.user.User;
@@ -10,6 +11,7 @@ import br.ifrn.semadec.repositories.UserRepository;
 @Service
 public class ReadAllUsers {
 
+    @Autowired
     private UserRepository userRepository;
 
     public List<User> execute() {
