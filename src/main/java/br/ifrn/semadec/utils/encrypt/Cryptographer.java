cbc795e27243e98f2598844570492b58fc675584
Lucas Jdev <lucas.jdev1@gmail.com>
2023-02-08 13:23:16 -0300

    criação de variável estática

--- src/main/java/br/ifrn/semadec/utils/encrypt/Cryptographer.java
+++ src/main/java/br/ifrn/semadec/utils/encrypt/Cryptographer.java
@@ -3,14 +3,19 @@ package br.ifrn.semadec.utils.encrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class Cryptographer {
-    
+
+    private static final BCryptPasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
+
     private Cryptographer() {
         throw new IllegalStateException("Utility class");
     }
 
     public static String encrypt(String password) {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        return passwordEncoder.encode(password);
+        return PASSWORD_ENCODER.encode(password);
+    }
+
+    public static boolean matches(String password, String encoded) {
+        return PASSWORD_ENCODER.matches(password, encoded);
     }
 
 }
