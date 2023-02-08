762c2b3775f379049af29bfb138d407a5391178e
Lucas Jdev <lucas.jdev1@gmail.com>
2023-02-08 13:42:35 -0300

    mudança na assinatura do método

--- src/main/java/br/ifrn/semadec/services/user/update/UpdateUser.java
+++ src/main/java/br/ifrn/semadec/services/user/update/UpdateUser.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.ifrn.semadec.dtos.user.input.UserInput;
+import br.ifrn.semadec.dtos.user.user_simple.input.UserInputUpdate;
 import br.ifrn.semadec.entities.user.User;
 import br.ifrn.semadec.exceptions.not_found.UserNotFoundException;
 import br.ifrn.semadec.repositories.UserRepository;
@@ -16,7 +16,8 @@ public class UpdateUser {
     @Autowired
     private UserRepository userRepository;
 
-    public User execute(final UUID userId, final UserInput input) {
+    public User execute(final UUID userId, final UserInputUpdate input) {
+
         return userRepository.findById(userId).map(user -> {
             user.setUsername(input.getUsername());
             user.setFullName(input.getFullName());
