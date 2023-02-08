ce8b3e1a9a78e0a4a306a893831a9283494276c2
Lucas Jdev <lucas.jdev1@gmail.com>
2023-02-08 13:20:47 -0300

    criação de input para redes sociais

---
+++ src/main/java/br/ifrn/semadec/dtos/user/user_simple/input/SocialNetworkInput.java
@@ -0,0 +1,13 @@
+package br.ifrn.semadec.dtos.user.user_simple.input;
+
+import lombok.Data;
+
+@Data
+public class SocialNetworkInput {
+
+    private String twitter;
+    private String facebook;
+    private String instagram;
+    private String tiktok;
+
+}
