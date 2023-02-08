0b489b9cb4590867966461700c1655da9e1ecab1
Lucas Jdev <lucas.jdev1@gmail.com>
2023-02-08 13:41:43 -0300

    aumento de complexidade para retorno de serviço

--- src/main/java/br/ifrn/semadec/resolvers/user/mutation/UserMutation.java
+++ src/main/java/br/ifrn/semadec/resolvers/user/mutation/UserMutation.java
@@ -7,11 +7,13 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
 
-import br.ifrn.semadec.dtos.user.input.UserInput;
-import br.ifrn.semadec.dtos.user.output.UserOutput;
+import br.ifrn.semadec.dtos.oauth.user.UserOauthInput;
+import br.ifrn.semadec.dtos.user.user_simple.input.UserInputUpdate;
+import br.ifrn.semadec.dtos.user.user_simple.output.UserOutput;
 import br.ifrn.semadec.entities.user.User;
 import br.ifrn.semadec.services.user.create.CreateUser;
 import br.ifrn.semadec.services.user.update.UpdateUser;
+import br.ifrn.semadec.services.user_provider.create.CreateUserProvider;
 import br.ifrn.semadec.utils.converter.ConvertEntityToDTO;
 
 @Controller
@@ -21,16 +23,22 @@ public class UserMutation {
     private CreateUser createUser;
 
     @Autowired
+    private CreateUserProvider createUserProvider;
+
+    @Autowired
     private UpdateUser updateUser;
 
-    @MutationMapping(name = "createUser")
-    public UserOutput save(@Argument final UserInput input) {
-        return createUser.execute(input);
+    @MutationMapping
+    public Object createUser(@Argument final UserOauthInput userInput) {
+        if (userInput.getUserProviderInput() != null) {
+            return createUserProvider.execute(userInput);
+        }
+        return createUser.execute(userInput.getUserInput());
     }
 
-    @MutationMapping(name = "updateUser")
-    public UserOutput update(@Argument final String id, @Argument final UserInput input) {
-        User user = updateUser.execute(UUID.fromString(id), input);
+    @MutationMapping
+    public UserOutput updateUser(@Argument final String id, @Argument final UserInputUpdate userInput) {
+        User user = updateUser.execute(UUID.fromString(id), userInput);
         return (UserOutput) ConvertEntityToDTO.convert(user, UserOutput.class);
     }
 
